06482d5f8dab6de919f16570a089997b8e95fef6
Dayvison <dayvisondapazpereira@outlook.com>
2022-11-30 11:53:52 -0300

    add login route

--- src/header.jsx
+++ src/header.jsx
@@ -79,7 +79,7 @@ navigate('/search')
           <button onClick={()=>navigate('/list')} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">List</button>
         </li>
         <li>
-          <button onClick={()=>navigate('/profile')} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><img className="w-10" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" /></button>
+          <button onClick={()=>navigate('/profile')} className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><img className="w-10" src="https://cdn-icons-png.flaticon.com/512/149/149071.png" onClick={()=>navigate('/login')} alt="" /></button>
         </li>
       </ul></div>
 </div>)
