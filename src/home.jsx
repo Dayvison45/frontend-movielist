89f8d8921a7a953a7a3ba7c682a2c4fd19b0cad9
Dayvison <dayvisondapazpereira@outlook.com>
2023-10-08 10:49:50 -0300

    add arrow in home

--- src/home.jsx
+++ src/home.jsx
@@ -31,19 +31,19 @@ loged===true?addList(x):navigate('/login')
 }
 
 function move(x,y){
-   console.log(x,y)
    let screenwidth = window.screen.width
    if(x==="one"){
-   if(y==="right" && oneLine+200 <=3*screenwidth){setOneLine(oneLine-screenwidth)}else{return}
-   if(y==="left" && oneLine-200 >=0){setOneLine(oneLine+screenwidth)}else{return}
+   if(y==="right" && oneLine >=3*-screenwidth){setOneLine(oneLine-screenwidth)}else
+   if(y==="left" && oneLine+200 <=0){setOneLine(oneLine+screenwidth)}else{return}
    }
    if(x==="two"){
-
+      if(y==="right" && twoLine >=3*-screenwidth){setTwoLine(twoLine-screenwidth)}else
+      if(y==="left" && twoLine+200 <=0){setTwoLine(twoLine+screenwidth)}else{return}
    }
    if(x==="three"){
-
+      if(y==="right" && threeLine >=3*-screenwidth){setthreeline(threeLine-screenwidth)}else
+      if(y==="left" && threeLine+200 <=0){setthreeline(threeLine+screenwidth)}else{return}
    }
-
 
 }
 
@@ -52,16 +52,13 @@ function genero(x){
   
    return result.name}
 
-function roll(){
-   setOne(one-400)
-}
 
 return(<div className=' w-screen h-screen overflow-x-hidden bg-slate-700 fixed'>
 <div className=''>{data.length===0?<Spinner/>:""}</div>
 
 
 {details[0]?
-   <div style={details[0].length<=0?{visibility:'hidden'}:{visibility:'visible'}} onClick={()=>setDetails([])} className='w-screen h-screen flex items-center justify-center bg-slate-700 fixed' >
+   <div style={details[0].length<=0?{visibility:'hidden'}:{visibility:'visible'}} onClick={()=>setDetails([])} className='z-10 w-screen h-screen flex items-center justify-center bg-slate-700 fixed' >
    <div className="text-black fixed w-full h-1/4 bg-white  flex mt-8 rounded-md shadow-2xl text-base  sm:h-1/3 sm:w-3/4 md:w-2/3 lg:w-3/5 xl:w-1/2 2xl:w-2/5 ">
    <img className="h-full rounded-md" src={"https://image.tmdb.org/t/p/w500/"+details[0].poster_path} alt="" />
   <div className="justify-around p-8 items-center text-xs sm:text-sm md:text-base lg:text-base xl:text-lg flex flex-col"><p>{details[0].original_title}</p>
@@ -72,19 +69,19 @@ return(<div className=' w-screen h-screen overflow-x-hidden bg-slate-700 fixed'>
 <Header></Header>
 
 
-<div className='w-screen flex '>
-<div className='flex xl:h-[60%] h-1/3  w-screen items-center absolute justify-between pr-5'>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
 <img onClick={()=>move("one","left")}className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 <img onClick={()=>move("one","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 </div>
-<div   style={{"margin-left":`${oneLine}px`}} className='w-1/6  flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
+<div   style={{"margin-left":`${oneLine}px`}} className='hover:transition-all flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
 
 </div>
+{data.length>1?data[0].map(e=><img  onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}
+</div>
 
-   
-{data.length>1?data[0].map(e=><img  onClick={()=>setDetails([e])} className='cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}</div>
-<div className='w-screen flex '>
-<div className='flex xl:h-[60%] h-1/3  w-screen items-center absolute justify-between pr-5'>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
 <img onClick={()=>move("two","left")} className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 <img onClick={()=>move("two","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 </div>
@@ -93,9 +90,9 @@ return(<div className=' w-screen h-screen overflow-x-hidden bg-slate-700 fixed'>
 </div>
 
    
-   {data.length>1?data[1].map(e=><img onClick={()=>setDetails([e])} className='cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):""}</div>
-   <div className='w-screen flex '>
-<div className='flex xl:h-[60%] h-1/3  w-screen items-center absolute justify-between pr-5'>
+   {data.length>1?data[1].map(e=><img onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):""}</div>
+   <div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
 <img onClick={()=>move("three","left")} className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 <img onClick={()=>move("three","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
 </div>
@@ -104,7 +101,7 @@ return(<div className=' w-screen h-screen overflow-x-hidden bg-slate-700 fixed'>
 </div>
 
    
-   {data.length>1?data[2].map(e=><img onClick={()=>setDetails([e])} className='cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):""}</div>
+   {data.length>1?data[2].map(e=><img onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):""}</div>
 
 
 </div>)
