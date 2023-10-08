4433642f6a7dad8759c6246e02b0025d5ab8f5cd
Dayvison <dayvisondapazpereira@outlook.com>
2023-10-08 11:04:26 -0300

    update series arrow pages

--- src/series.jsx
+++ src/series.jsx
@@ -9,6 +9,10 @@ export default function Series(){
    const [details,setDetails] = useState([])
    const [data,setData] = useState([]) 
    const {addList,ifLogin} = useContext(UserContext)
+   const [oneLine,setOneLine] = useState(0)
+   const [twoLine,setTwoLine] = useState(0)
+   const [threeLine,setthreeline] = useState(0)
+   const [fourLine,setFourline] = useState(0)
    useEffect(() => { run()
    async function run(){
    await axios.get('https://movies-0533.onrender.com/series').then(response=>setData([response.data[0].results, response.data[1].results,response.data[2].results,response.data[3].results]))}}, []);  
@@ -22,7 +26,26 @@ export default function Series(){
          loged===true?addList(x):navigate('/login')
          
          }
-
+         function move(x,y){
+            let screenwidth = window.screen.width
+            if(x==="one"){
+            if(y==="right" && oneLine >=3*-screenwidth){setOneLine(oneLine-screenwidth)}else
+            if(y==="left" && oneLine+200 <=0){setOneLine(oneLine+screenwidth)}else{return}
+            }
+            if(x==="two"){
+               if(y==="right" && twoLine >=3*-screenwidth){setTwoLine(twoLine-screenwidth)}else
+               if(y==="left" && twoLine+200 <=0){setTwoLine(twoLine+screenwidth)}else{return}
+            }
+            if(x==="three"){
+               if(y==="right" && threeLine >=3*-screenwidth){setthreeline(threeLine-screenwidth)}else
+               if(y==="left" && threeLine+200 <=0){setthreeline(threeLine+screenwidth)}else{return}
+            }
+            if(x==="four"){
+               if(y==="right" && fourLine >=3*-screenwidth){setFourline(fourLine-screenwidth)}else
+               if(y==="left" && fourLine+200 <=0){setFourline(fourLine+screenwidth)}else{return}
+            }
+         }
+         
 return(<div className='w-screen overflow-hidden bg-slate-700 text-white' >
    <div className=''>{data.length===0?<Spinner/>:""}</div>
 {details[0]?
@@ -35,17 +58,51 @@ return(<div className='w-screen overflow-hidden bg-slate-700 text-white' >
    </div></div>
    </div>:""}
 <Header></Header>
+<h1 className=' w-screen text-center '>Series mais populares</h1>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
+<img onClick={()=>move("one","left")}className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+<img onClick={()=>move("one","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+</div>
+<div   style={{"margin-left":`${oneLine}px`}} className='hover:transition-all flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
 
-<h1 className=' w-screen text-center '>filmes mais populares</h1>
-<ScrollContainer className='flex  overflow-x-hidden max-[640px]:overflow-x-scroll'>{data.length>1?data[0].map(e=><img  onClick={()=>setDetails([e])} className='cursor-pointer w-1/4 shadow-2xl m-8 scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):" "}</ScrollContainer>
+</div>
+{data.length>1?data[0].map(e=><img  onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}
+</div>
+<h1 className='w-screen text-center'>Series de ações</h1>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
+<img onClick={()=>move("two","left")}className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+<img onClick={()=>move("two","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+</div>
+<div   style={{"margin-left":`${twoLine}px`}} className='hover:transition-all flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
 
-<h1 className='w-screen text-center'>filmes de ações</h1>
-<ScrollContainer className='flex  overflow-x-hidden max-[640px]:overflow-x-scroll'>{data.length>1?data[1].map(e=><img  onClick={()=>setDetails([e])} className='cursor-pointer w-1/4 shadow-2xl m-8 scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):" "}</ScrollContainer>
-<h1 className='w-screen text-center'>Filmes de aventuras</h1>
-<ScrollContainer className='flex  overflow-x-hidden max-[640px]:overflow-x-scroll'>{data.length>1?data[2].map(e=><img  onClick={()=>setDetails([e])} className='cursor-pointer w-1/4 shadow-2xl m-8 scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):" "}</ScrollContainer>
-<h1 className='w-screen text-center'>filmes de animações</h1>
-<ScrollContainer className='flex  overflow-x-hidden max-[640px]:overflow-x-scroll'>{data.length>1?data[3].map(e=><img  onClick={()=>setDetails([e])} className='cursor-pointer w-1/4 shadow-2xl m-8 scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):" "}</ScrollContainer>
+</div>
+{data.length>1?data[1].map(e=><img  onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}
+</div>
 
+<h1 className='w-screen text-center'>Series de aventuras</h1>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
+<img onClick={()=>move("three","left")}className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+<img onClick={()=>move("three","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+</div>
+<div   style={{"margin-left":`${threeLine}px`}} className='hover:transition-all flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
+
+</div>
+{data.length>1?data[2].map(e=><img  onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}
+</div>
+<h1 className='w-screen text-center'>Series de animações</h1>
+<div className='w-screen flex items-center'>
+<div className='z-0 h-[5%] w-screen absolute flex justify-between items-center'>
+<img onClick={()=>move("four","left")}className='xl:w-14 xl:h-14 sm:w-8 sm:h-8 rotate-180' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+<img onClick={()=>move("four","right")} className='xl:w-14 xl:h-14  w-14 h-14 sm:w-8 sm:h-8' src="https://cdn-icons-png.flaticon.com/512/6775/6775949.png" alt="" />
+</div>
+<div   style={{"margin-left":`${fourLine}px`}} className='hover:transition-all flex scroll overflow-x-hidden max-[640px]:overflow-x-scroll'>
+
+</div>
+{data.length>1?data[3].map(e=><img  onClick={()=>setDetails([e])} className='hover:bg-blue-500 cursor-pointer p-4 w-1/4 shadow-2xl  scrollbar-hide' src={"https://image.tmdb.org/t/p/original/"+e.poster_path} alt="" />):''}
+</div>
 
 </div>)
 
